b292c306b8ffa4e0679fde7b96e9ded8c66da7e1
Laura Thornburg <lthornburg21@gmail.com>
2024-09-27 17:06:04 -0500

    Sending project to git repository

---
+++ src/main/java/com/javaprojects/laura_thornburg_formattedwebapp/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.javaprojects.laura_thornburg_formattedwebapp;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
